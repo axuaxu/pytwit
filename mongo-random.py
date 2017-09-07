5812e68ebdf330ddd2d2196b02b04c697676f43d
axuaxu <axu0110@gmail.com>
2017-09-06 21:38:37 -0400

    3

--- mongo-random.py
+++ mongo-random.py
@@ -1,6 +1,7 @@
 #http://api.mongodb.com/python/current/index.html
 #https://www.mongodb.com/blog/post/how-to-perform-random-queries-on-mongodb
 #http://www.bogotobogo.com/python/MongoDB_PyMongo/python_MongoDB_pyMongo_tutorial_connecting_accessing.php
+#https://www.mongodb.com/blog/post/getting-started-with-python-and-mongodb
 from pymongo import MongoClient
 
 
@@ -9,5 +10,7 @@ db = client.flist
 coll = db.flist
  
 print(coll.find_one())
-print(db.version)
+#print(db.version)
 #db.coll.aggregate([{$sample: {size: 1}}])
+winner = [ d for d in coll.aggregate([{'$sample': {'size': 1 }}])][0]
+print(winner)
